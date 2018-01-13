18b604e310fb0acf2cde63837f0cad28d36c0f0c
prafullakumarsahu <prafullakumarsahu001@gmail.com>
2018-01-13 09:26:07 +0530

    Allows user to change answer

--- app/Http/Controllers/AnswerController.php
+++ app/Http/Controllers/AnswerController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 
+use DB;
 use App\Exam;
 use App\Question;
 use App\Answer;
@@ -14,30 +15,33 @@ class AnswerController extends Controller
 {
     public function store($exam_id, $question_id)
     {
+        //Todo
+        //Could be enhanced
 
-        //Check if already attained
-        //if yes update
-        // $userQuestionAnswer = new UserQuestionAnswer();
-        // $userQuestionAnswer->user_id = Auth::id();
-        // $userQuestionAnswer->exam_id = $exam_id;
-        // $userQuestionAnswer->question_id = $question_id;
-        // $userQuestionAnswer->answer_id = request('answer');
-        // $userQuestionAnswer->save();
+        $answer = DB::table('user_question_answers')->where(
+            [
+                ['exam_id', '=', $exam_id],
+                ['question_id', '=', $question_id]
+            ]
+        )->get()->last();
+
+        if ( count($answer) ){
+            $updated = DB::table('user_question_answers')
+                ->where(
+                [
+                    ['exam_id', '=', $exam_id],
+                    ['question_id', '=', $question_id]
+                ]
+            )->update(['answer_id' => request('answer')]);
+        } else {
+            $userQuestionAnswer = new UserQuestionAnswer();
+            $userQuestionAnswer->user_id = Auth::id();
+            $userQuestionAnswer->exam_id = $exam_id;
+            $userQuestionAnswer->question_id = $question_id;
+            $userQuestionAnswer->answer_id = request('answer');
+            $userQuestionAnswer->save();
+        }
         
-    	UserQuestionAnswer::create([
-    		'user_id' => Auth::id(),
-    		'exam_id' => $exam_id,
-    		'question_id' => $question_id,
-    		'answer_id' => request('answer')
-    	]);
-    	
-    	// var_dump($exam_id);
-    	// var_dump($question_id);
-    	// var_dump(Auth::id());
-    	// dd(request(['answer']));
-
-    	//Update 
-    	//redirect
     	return redirect()->back();
     }
 }
